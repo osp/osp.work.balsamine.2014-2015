ae5c9e114d8e119d80cdb82612b62af6778ddb8e
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-06-24 23:40:53 +0200

    scripts manquants au process d'importations des fontes automatique dans Fontforge

---
+++ scripts/UmePlume/make-ume.sh
@@ -0,0 +1,25 @@
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  30 1
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  45 2
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  65 3
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  90 4
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  120 5
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  158 6
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  200 7
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  260 8
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  330 9
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  410 10
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  500 11
+#python2 expand-stroke-font.py  "Ume P Mincho" "UmePlumePMincho"  600 12
+
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  30 1
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  45 2
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  65 3
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  90 4
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  120 5
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  158 6
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  200 7
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  260 8
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  330 9
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  410 10
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  500 11
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  600 12
