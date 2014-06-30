7b0363c0281f8bb603f83d5dc479dc676df0aa2d
Stéphanie Vilayphiou <stephanie@stdin.fr>
2014-06-30 22:29:08 +0200

    retouché les flèches ondulées de la fonte

--- scripts/UmePlume/make-ume.sh
+++ scripts/UmePlume/make-ume.sh
@@ -18,8 +18,9 @@ python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  90 4
 python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  120 5
 python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  158 6
 python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  200 7
-python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  260 8
-python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  330 9
-python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  410 10
-python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  500 11
-python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  600 12
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  220 8
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  260 9
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  300 10
+python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  410 11
+#python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  500 11
+#python2 expand-stroke-font.py  "Ume P Gothic Balsa" "UmePlumePGothic"  600 12
